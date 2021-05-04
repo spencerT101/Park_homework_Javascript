e301ab6658d604078746151b59641557fef43ebc
Spencer <Spencer.Taber@yahoo.com>
2021-05-04 21:12:19 +0100

    add dinosaur to collection test passed

--- hw_tdd_jurassic_park_start/models/park.js
+++ hw_tdd_jurassic_park_start/models/park.js
@@ -3,6 +3,13 @@ const Park = function (name, price) {
     this.price = price;
     this.dinosaur_collection = [];
   }
+
+  Park.prototype.numberOfDinosaurs = function () {
+      return this.dinosaur_collection.length;
+  }
   
+  Park.prototype.addDinosaur = function(dinosaur){
+      this.dinosaur_collection.push(dinosaur)
+  }
 
   module.exports = Park;
--- hw_tdd_jurassic_park_start/specs/park_spec.js
+++ hw_tdd_jurassic_park_start/specs/park_spec.js
@@ -22,12 +22,22 @@ describe('Park', function() {
   
   describe('dinosaur_collection', function(){
 
+    let dinosaur;
+
+    beforeEach(function () {
+      dinosaur_1 = new Dinosaur('t-rex', 'carnivore', 50) 
+    })
+
     it('should have a collection of dinosaurs', function (){
       const actual = park_1.dinosaur_collection
       assert.deepStrictEqual(actual, [])
   });
-
-  it('should be able to add a dinosaur to its collection');
+  
+    it('should be able to add a dinosaur to its collection', function(){
+      park_1.addDinosaur(dinosaur_1);
+      const actual = park_1.numberOfDinosaurs();
+      assert.strictEqual(actual, 1);
+    });
 
   it('should be able to remove a dinosaur from its collection');
 
