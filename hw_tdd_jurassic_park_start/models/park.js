58bb538823976e50c68c6ce28c9ab67dd54eef37
Spencer <Spencer.Taber@yahoo.com>
2021-05-04 20:38:46 +0100

    beginning tests on Park

---
+++ hw_tdd_jurassic_park_start/models/park.js
@@ -0,0 +1,8 @@
+const Park = function (name, price) {
+    this.name = name;
+    this.price = price;
+    this.dinosaur_collection = [];
+  }
+  
+
+  module.exports = Park;
---
+++ hw_tdd_jurassic_park_start/specs/park_spec.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+const Park = require('../models/park.js');
+const Dinosaur = require('../models/dinosaur.js');
+
+describe('Park', function() {
+  let = park
+  beforeEach(function () {
+    park_1 = new Park('Jurassic Park','£50' )
+
+  })
+
+  it('should have a name', function);
+
+  it('should have a ticket price');
+
+  it('should have a collection of dinosaurs');
+
+  it('should be able to add a dinosaur to its collection');
+
+  it('should be able to remove a dinosaur from its collection');
+
+  it('should be able to find the dinosaur that attracts the most visitors');
+
+  it('should be able to find all dinosaurs of a particular species');
+
+  it('should be able to calculate the total number of visitors per day');
+
+  it('should be able to calculate the total number of visitors per year');
+
+  it('should be able to calculate total revenue for one year');
+
+});
